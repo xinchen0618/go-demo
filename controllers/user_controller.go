ead4f5542a749ec2c132e04c86c8cdb230e8a288
chenxin <chen.xin@hbtown.com>
2021-05-21 13:44:40 +0800

    panic

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gohouse/gorose/v2"
 	"go-test/config"
-	"log"
 	"time"
 )
 
@@ -13,41 +12,33 @@ func GetUsers(c *gin.Context) {
 	key := "redis:users"
 	var res []gorose.Data
 	resCache, err := config.Cache.Get(config.Ctx, key).Result()
-	if err == nil {    // 缓存存在
+	if err == nil { // 缓存存在
 		err = json.Unmarshal([]byte(resCache), &res)
 		if err != nil {
-			log.Println(err)
-			c.JSON(500, gin.H{"status": "InternalError", "message": "服务异常, 请稍后重试"})
-			return
+			panic(err)
 		}
 
-	} else {    // 缓存不存在
-		res, err = config.Db.Query("SELECT * FROM t_users LIMIT 12")
+	} else { // 缓存不存在
+		res, err = config.Db.Query("SELECT * FROM t_users2 LIMIT 12")
 		if err != nil {
-			log.Println(err)
-			c.JSON(500, gin.H{"status": "InternalError", "message": "服务异常, 请稍后重试"})
-			return
+			panic(err)
 		}
 
 		data, err := json.Marshal(res)
 		if err != nil {
-			log.Println(err)
-			c.JSON(500, gin.H{"status": "InternalError", "message": "服务异常, 请稍后重试"})
-			return
+			panic(err)
 		}
-		err = config.Cache.Set(config.Ctx, key, data, 10 * time.Second).Err()
+		err = config.Cache.Set(config.Ctx, key, data, 10*time.Second).Err()
 		if err != nil {
-			log.Println(err)
-			c.JSON(500, gin.H{"status": "InternalError", "message": "服务异常, 请稍后重试"})
-			return
+			panic(err)
 		}
 	}
 
 	c.JSON(200, gin.H{
-		"page": 1,
-		"per_page": 12,
-		"total_pages": 1,
+		"page":         1,
+		"per_page":     12,
+		"total_pages":  1,
 		"total_counts": 1,
-		"items": res,
+		"items":        res,
 	})
 }
--- main.go
+++ main.go
@@ -6,16 +6,26 @@ import (
 	"log"
 )
 
-func main() {
-	/* Recover */
-	defer func() {
-		if err := recover(); err != nil {
-			log.Println(err)
-		}
-	}()
+/* Panic处理 */
+func recovery() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println(err)
+				c.JSON(500, gin.H{"status": "InternalError", "message": "服务异常, 请稍后重试"})
+			}
+		}()
 
+		c.Next()
+	}
+}
+
+func main() {
 	/* Run gin */
 	r := gin.Default()
+
+	// Panic处理
+	r.Use(recovery())
 
 	// 加载路由
 	routers.LoadUser(r)
