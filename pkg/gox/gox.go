45698e912ac8e6c6a087e9a98a2d8c321782ca73
chenxin <chen.xin@hbtown.com>
2021-08-04 09:27:22 +0800

    new layout

--- pkg/gox/gox.go
+++ pkg/gox/gox.go
@@ -2,12 +2,18 @@
 package gox
 
 // InSlice 元素是否在切片中
-//	支持string/int64/float64类型
+//	支持int64/string/float64类型
 //	@param needle interface{}
 //	@param haystack interface{}
 //	@return bool
 func InSlice(needle interface{}, haystack interface{}) bool {
 	switch key := needle.(type) {
+	case int64:
+		for _, item := range haystack.([]int64) {
+			if key == item {
+				return true
+			}
+		}
 	case string:
 		for _, item := range haystack.([]string) {
 			if key == item {
@@ -16,12 +22,6 @@ func InSlice(needle interface{}, haystack interface{}) bool {
 		}
 	case float64:
 		for _, item := range haystack.([]float64) {
-			if key == item {
-				return true
-			}
-		}
-	case int64:
-		for _, item := range haystack.([]int64) {
 			if key == item {
 				return true
 			}
