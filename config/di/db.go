19dcf732a41aafbb08a5b947a147c5f83048cf5e
chenxin <chen.xin@hbtown.com>
2021-08-25 14:11:03 +0800

    db

--- config/di/db.go
+++ config/di/db.go
@@ -5,9 +5,11 @@ import (
 	"go-demo/config"
 	"os"
 	"sync"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gohouse/gorose/v2"
+	"github.com/golang-module/carbon"
 )
 
 // mysql
@@ -16,22 +18,41 @@ var (
 	dbOnce   sync.Once
 )
 
+// print SQL
+type sqlLogger struct {
+}
+
+func (sqlLogger) Sql(sqlStr string, runtime time.Duration) {
+	fmt.Printf("[SQL] [%s] %s --- %s\n", carbon.Now().ToDateTimeString(), runtime.String(), sqlStr)
+}
+func (sqlLogger) Slow(sqlStr string, runtime time.Duration) {
+}
+func (sqlLogger) Error(msg string) {
+}
+func (sqlLogger) EnableSqlLog() bool {
+	return true
+}
+func (sqlLogger) EnableErrorLog() bool {
+	return false
+}
+func (sqlLogger) EnableSlowLog() float64 {
+	return 0
+}
+
 func Db() gorose.IOrm {
 	dbOnce.Do(func() {
 		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s",
 			config.Get("mysql_username"), config.Get("mysql_password"), config.Get("mysql_host"),
 			config.Get("mysql_port"), config.Get("mysql_dbname"), config.Get("mysql_charset"))
 		var err error
-		dbEngine, err = gorose.Open(&gorose.Config{Driver: "mysql", Dsn: dsn, SetMaxOpenConns: 100, SetMaxIdleConns: 100})
+		dbEngine, err = gorose.Open(&gorose.Config{Driver: "mysql", Dsn: dsn, SetMaxOpenConns: 100, SetMaxIdleConns: 10})
 		if err != nil {
 			panic(err)
 		}
 
+		// print SQL to console
 		if "dev" == os.Getenv("RUNTIME_ENV") || "testing" == os.Getenv("RUNTIME_ENV") {
-			dbEngine.SetLogger(gorose.NewLogger(&gorose.LogOption{
-				FilePath:     "./tmp",
-				EnableSqlLog: true,
-			}))
+			dbEngine.SetLogger(sqlLogger{})
 		}
 	})
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-demo
 go 1.16
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
@@ -12,6 +13,7 @@ require (
 	github.com/go-redis/redis/v8 v8.11.3
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gohouse/gorose/v2 v2.1.12
+	github.com/golang-module/carbon v1.5.3
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
