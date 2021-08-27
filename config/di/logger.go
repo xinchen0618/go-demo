6dbcdde7d0dc4130ca885f80ec3fb4bc10d20a19
chenxin <chen.xin@hbtown.com>
2021-08-27 11:15:03 +0800

    log

--- config/di/logger.go
+++ config/di/logger.go
@@ -3,31 +3,30 @@ package di
 import (
 	"go-demo/config"
 	"os"
-	"sync"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var (
-	logger     *zap.Logger
-	loggerOnce sync.Once
+	logger *zap.Logger
 )
 
-func Logger() *zap.Logger {
-	loggerOnce.Do(func() {
-		logFile, err := os.OpenFile(config.GetString("error_log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0664)
-		if err != nil {
-			panic(err)
-		}
-		writeSyncer := zapcore.AddSync(logFile)
-		encoderConfig := zap.NewProductionEncoderConfig()
-		encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-		encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-		encoder := zapcore.NewConsoleEncoder(encoderConfig)
-		zapCore := zapcore.NewCore(encoder, zapcore.NewMultiWriteSyncer(writeSyncer, zapcore.AddSync(os.Stdout)), zapcore.DebugLevel) // 输出到console和文件
-		logger = zap.New(zapCore, zap.AddStacktrace(zapcore.ErrorLevel))                                                              // 错误日志记录栈信息
-	})
+// 日志服务最为基础, 日志初始化失败, 程序不允许启动
+func init() {
+	logFile, err := os.OpenFile(config.GetString("error_log"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0664)
+	if err != nil {
+		panic(err)
+	}
+	writeSyncer := zapcore.AddSync(logFile)
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	encoder := zapcore.NewConsoleEncoder(encoderConfig)
+	zapCore := zapcore.NewCore(encoder, zapcore.NewMultiWriteSyncer(writeSyncer, zapcore.AddSync(os.Stdout)), zapcore.DebugLevel) // 输出到console和文件
+	logger = zap.New(zapCore, zap.AddStacktrace(zapcore.ErrorLevel))                                                              // 错误日志记录栈信息
+}
 
+func Logger() *zap.Logger {
 	return logger
 }
