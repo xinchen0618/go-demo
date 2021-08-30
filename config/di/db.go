8faa455d8c1ae3bed8974817504a2f8a4295460f
chenxin <chen.xin@hbtown.com>
2021-08-30 13:46:28 +0800

    db

--- config/di/db.go
+++ config/di/db.go
@@ -41,25 +41,25 @@ func (sqlLogger) EnableSlowLog() float64 {
 }
 
 func Db() gorose.IOrm {
-	dbMutex.Lock()
-	if false == dbOk {
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s",
-			config.Get("mysql_username"), config.Get("mysql_password"), config.Get("mysql_host"),
-			config.Get("mysql_port"), config.Get("mysql_dbname"), config.Get("mysql_charset"))
-		var err error
-		dbEngine, err = gorose.Open(&gorose.Config{Driver: "mysql", Dsn: dsn, SetMaxOpenConns: 100, SetMaxIdleConns: 100})
-		if err != nil {
-			dbMutex.Unlock()
-			Logger().Error(err.Error())
-			return nil
+	func() {
+		dbMutex.Lock() // mutex锁与解锁务必成对出现
+		defer dbMutex.Unlock()
+		if !dbOk {
+			dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s",
+				config.Get("mysql_username"), config.Get("mysql_password"), config.Get("mysql_host"),
+				config.Get("mysql_port"), config.Get("mysql_dbname"), config.Get("mysql_charset"))
+			var err error
+			dbEngine, err = gorose.Open(&gorose.Config{Driver: "mysql", Dsn: dsn, SetMaxOpenConns: 100, SetMaxIdleConns: 100})
+			if err != nil {
+				Logger().Error(err.Error())
+				return
+			}
+			if "dev" == os.Getenv("RUNTIME_ENV") || "testing" == os.Getenv("RUNTIME_ENV") { // print SQL to console
+				dbEngine.SetLogger(sqlLogger{})
+			}
+			dbOk = true
 		}
-		if "dev" == os.Getenv("RUNTIME_ENV") || "testing" == os.Getenv("RUNTIME_ENV") { // print SQL to console
-			dbEngine.SetLogger(sqlLogger{})
-		}
-
-		dbOk = true
-	}
-	dbMutex.Unlock()
+	}()
 
 	return dbEngine.NewOrm()
 }
