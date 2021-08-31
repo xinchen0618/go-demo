a0299f93dd1e7a360a4c56f6b48e6ad8e33b07a0
chenxin <chen.xin@hbtown.com>
2021-08-31 15:50:58 +0800

    gox

--- pkg/gox/gox.go
+++ pkg/gox/gox.go
@@ -39,3 +39,13 @@ func InSlice(needle interface{}, haystack interface{}) bool {
 
 	return false
 }
+
+// PrintMap 向console打印map
+//	@param m map[string]interface{}
+func PrintMap(m map[string]interface{}) {
+	fmt.Println("{")
+	for k, v := range m {
+		fmt.Printf("\t%s: %T %#v\n", k, v, v)
+	}
+	fmt.Println("}")
+}
