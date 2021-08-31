d468e856712779f2c410eb8cbf60f24da9f30af2
chenxin <chen.xin@hbtown.com>
2021-08-31 13:44:50 +0800

    middleware

--- internal/middleware/user_auth.go
+++ internal/middleware/user_auth.go
@@ -14,51 +14,53 @@ import (
 )
 
 // UserAuth 用户登录
-//	@param c *gin.Context
-func UserAuth(c *gin.Context) {
-	tokenString := c.Request.Header.Get("Authorization") // Authorization: Bearer <token>
-	if !strings.HasPrefix(tokenString, "Bearer ") {
-		c.JSON(401, gin.H{"code": "UserUnauthorized", "message": "用户未登录或登录已过期, 请重新登录"})
-		c.Abort()
-		return
-	}
-	tokenString = tokenString[7:]
+//	@return gin.HandlerFunc
+func UserAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.Request.Header.Get("Authorization") // Authorization: Bearer <token>
+		if !strings.HasPrefix(tokenString, "Bearer ") {
+			c.JSON(401, gin.H{"code": "UserUnauthorized", "message": "用户未登录或登录已过期, 请重新登录"})
+			c.Abort()
+			return
+		}
+		tokenString = tokenString[7:]
 
-	// JWT
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte(config.GetString("jwt_secret")), nil
-	})
-	if err != nil {
-		c.JSON(401, gin.H{"code": "UserUnauthorized", "message": "用户未登录或登录已过期, 请重新登录"})
-		c.Abort()
-		return
-	}
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid { // redis白名单校验
-		tokenAtoms := strings.Split(tokenString, ".")
-		key := "jwt:" + claims["jti"].(string) + ":" + tokenAtoms[2]
-		_, err := di.JwtRedis().Get(context.Background(), key).Result()
+		// JWT
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(config.GetString("jwt_secret")), nil
+		})
 		if err != nil {
-			if redis.Nil == err {
-				c.JSON(401, gin.H{"code": "UserUnauthorized", "message": "用户未登录或登录已过期, 请重新登录"})
+			c.JSON(401, gin.H{"code": "UserUnauthorized", "message": "用户未登录或登录已过期, 请重新登录"})
+			c.Abort()
+			return
+		}
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid { // redis白名单校验
+			tokenAtoms := strings.Split(tokenString, ".")
+			key := "jwt:" + claims["jti"].(string) + ":" + tokenAtoms[2]
+			_, err := di.JwtRedis().Get(context.Background(), key).Result()
+			if err != nil {
+				if redis.Nil == err {
+					c.JSON(401, gin.H{"code": "UserUnauthorized", "message": "用户未登录或登录已过期, 请重新登录"})
+					c.Abort()
+					return
+				}
+				ginx.InternalError(c, err) // redis服务异常
 				c.Abort()
 				return
 			}
-			ginx.InternalError(c, err) // redis服务异常
-			c.Abort()
-			return
-		}
-		userId, err := strconv.ParseInt(claims["jti"].(string), 10, 64)
-		if err != nil {
-			ginx.InternalError(c, err)
-			c.Abort()
-			return
-		}
-		c.Set("userId", userId) // 后续的处理函数可以用过c.GetInt64("userId")来获取当前请求的用户信息
-		c.Next()
+			userId, err := strconv.ParseInt(claims["jti"].(string), 10, 64)
+			if err != nil {
+				ginx.InternalError(c, err)
+				c.Abort()
+				return
+			}
+			c.Set("userId", userId) // 后续的处理函数可以用过c.GetInt64("userId")来获取当前请求的用户信息
+			c.Next()
 
-	} else {
-		c.JSON(401, gin.H{"code": "UserUnauthorized", "message": "用户未登录或登录已过期, 请重新登录"})
-		c.Abort()
-		return
+		} else {
+			c.JSON(401, gin.H{"code": "UserUnauthorized", "message": "用户未登录或登录已过期, 请重新登录"})
+			c.Abort()
+			return
+		}
 	}
 }
--- internal/router/account_router.go
+++ internal/router/account_router.go
@@ -12,7 +12,7 @@ func LoadAccount(r *gin.Engine) {
 	accountGroup := r.Group("/account")
 	{
 		accountGroup.POST("/v1/login", controller.AccountController.PostUserLogin)
-		accountGroup.DELETE("/v1/logout", middleware.UserAuth, controller.AccountController.DeleteUserLogout)
+		accountGroup.DELETE("/v1/logout", middleware.UserAuth(), controller.AccountController.DeleteUserLogout)
 
 		accountGroup.GET("/v1/users", controller.AccountController.GetUsers)
 		accountGroup.GET("/v1/users/:user_id", controller.AccountController.GetUsersById)
