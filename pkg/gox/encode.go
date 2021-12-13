b2e17efe5839c109b576450205db580d8f28f6fd
chenxin <chen.xin@hbtown.com>
2021-12-13 11:24:35 +0800

    gox

--- pkg/gox/encode.go
+++ pkg/gox/encode.go
@@ -4,7 +4,6 @@ import (
 	"crypto/md5"
 	"encoding/json"
 	"fmt"
-	"io"
 
 	"go.uber.org/zap"
 )
@@ -12,14 +11,8 @@ import (
 // Md5 字符串md5
 //	@param str string
 //	@return string
-//	@return error
-func Md5(s string) (string, error) {
-	h := md5.New()
-	if _, err := io.WriteString(h, s); err != nil {
-		zap.L().Error(err.Error())
-		return "", err
-	}
-	return fmt.Sprintf("%x", h.Sum(nil)), nil
+func Md5(s string) string {
+	return fmt.Sprintf("%x", md5.Sum([]byte(s)))
 }
 
 // Md5x 非String数据md5
@@ -33,5 +26,5 @@ func Md5x(i interface{}) (string, error) {
 		zap.L().Error(err.Error())
 		return "", err
 	}
-	return Md5(string(iBytes))
+	return Md5(string(iBytes)), nil
 }
