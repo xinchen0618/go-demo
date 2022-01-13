b15cc244238718ccce76a69bf2d80da31f6a48d4
chenxin <chen.xin@hbtown.com>
2022-01-13 09:50:08 +0800

    ginx

--- pkg/ginx/response.go
+++ pkg/ginx/response.go
@@ -8,7 +8,7 @@ import (
 // Success 向客户端输出成功信息
 //	@param c *gin.Context
 //	@param httpCode int
-//	@param obj ...interface{} 选填, 数据会json编码输出给客户端
+//	@param obj ...interface{} 选填, 数据会json编码返回给客户端
 func Success(c *gin.Context, httpCode int, obj ...interface{}) {
 	if len(obj) > 0 {
 		c.JSON(httpCode, obj[0])
