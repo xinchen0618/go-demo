7bdd4a3f7c7657df338610937ee6471aab44eee4
chenxin <chen.xin@hbtown.com>
2022-01-18 14:40:50 +0800

    gox

--- pkg/gox/string.go
+++ pkg/gox/string.go
@@ -1,5 +1,7 @@
 package gox
 
+import "unicode/utf8"
+
 // AddSlashes 使用反斜线引用字符串
 //  @param str string
 //  @return string
@@ -16,4 +18,37 @@ func AddSlashes(str string) string {
 		}
 	}
 	return string(tmpRune)
+}
+
+// Substr 返回字符串的子串
+//  中英文皆适用
+//  @param s string
+//  @param offset int
+// 	  若offset为非负数, 返回的子串将从字符串的offset位置开始, 从0开始计算.
+//	  若offset为负数，返回的子串将从字符串结尾处向前数第offset个字符开始.
+//	  若offset大于字符串长度，返回空字符串.
+//  @param length int 若length大于字符串长度, 取最大可取长度
+//  @return string
+func Substr(s string, offset, length int) string {
+	counts := utf8.RuneCountInString(s)
+	if 0 == counts {
+		return ""
+	}
+	if offset >= length {
+		return ""
+	}
+
+	if offset < 0 {
+		offset = counts + offset
+		if offset < 0 {
+			offset = 0
+		}
+	}
+
+	end := offset + length
+	if end > counts {
+		end = counts
+	}
+
+	return string([]rune(s)[offset:end])
 }
