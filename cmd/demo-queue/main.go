cd35bd47c3b16c9a3d175ad691db146337bf2870
chenxin <chen.xin@hbtown.com>
2022-01-20 16:24:23 +0800

    queue

--- cmd/demo-queue/main.go
+++ cmd/demo-queue/main.go
@@ -14,11 +14,12 @@ func loggingMiddleware(h asynq.Handler) asynq.Handler {
 	return asynq.HandlerFunc(func(ctx context.Context, t *asynq.Task) error {
 		start := time.Now()
 		log.Printf("Start processing %q --- %s", t.Type(), t.Payload())
-		err := h.ProcessTask(ctx, t)
-		if err != nil {
+
+		if err := h.ProcessTask(ctx, t); err != nil {
 			log.Printf("Processing err %q --- %s, %v", t.Type(), t.Payload(), err)
 			return err
 		}
+
 		log.Printf("Finished processing %q --- %s, Elapsed Time = %v", t.Type(), t.Payload(), time.Since(start))
 		return nil
 	})
