96937916703b263218a1c7a70029d8f1c07a8594
chenxin <chen.xin@hbtown.com>
2022-03-07 17:02:57 +0800

    config

--- config/prod.go
+++ config/prod.go
@@ -10,7 +10,7 @@ func init() {
 		// 运行端口
 		"server_port": 8081,
 
-		// JWT密钥, JWT配套有白名单动能不必担心秘钥泄露的问题
+		// JWT密钥, JWT配套有白名单功能不必担心秘钥泄露的问题
 		"jwt_secret": "btRZ5QHXX9VjfYhfGGHdCTcWiwQ6WFJXq9ZCwdqZwzk2ZfhceM9K3V5UGKsYLd9m",
 	}
 }
--- config/testing.go
+++ config/testing.go
@@ -10,7 +10,7 @@ func init() {
 		// 运行端口
 		"server_port": 8080,
 
-		// JWT密钥, JWT配套有白名单动能不必担心秘钥泄露的问题
+		// JWT密钥, JWT配套有白名单功能不必担心秘钥泄露的问题
 		"jwt_secret": "Xx4KJQ2AguFL5gWurcRJvVfDC5a2itLi53vFJN9wthYkrxtQbdeRDkWTHzAjnn5n",
 
 		// SQL日志
