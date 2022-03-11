b270e68555ee9d63c589322ee1a319fd6b10ce7b
chenxin <chen.xin@hbtown.com>
2022-03-11 14:34:33 +0800

    ginx

--- pkg/gox/map.go
+++ pkg/gox/map.go
@@ -2,7 +2,10 @@
 package gox
 
 import (
+	"encoding/json"
 	"fmt"
+
+	"go.uber.org/zap"
 )
 
 // PrintMap 向console打印map
@@ -13,4 +16,22 @@ func PrintMap(m map[string]interface{}) {
 		fmt.Printf("\t%s: %T %#v\n", k, v, v)
 	}
 	fmt.Println("}")
+}
+
+// Map2struct map转struct
+//  @param m map[string]interface{}
+//  @param sp interface{} 接收结果结构体的指针
+//  @return error
+func Map2struct(m map[string]interface{}, sp interface{}) error {
+	b, err := json.Marshal(m)
+	if err != nil {
+		zap.L().Error(err.Error())
+		return err
+	}
+	if err := json.Unmarshal(b, sp); err != nil {
+		zap.L().Error(err.Error())
+		return err
+	}
+
+	return nil
 }
