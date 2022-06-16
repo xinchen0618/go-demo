ba9fe0d3f7894c1dd182bac2876de533ad26c3b0
chenxin <chen.xin@hbtown.com>
2022-06-16 16:51:49 +0800

    queue

--- internal/task/user.go
+++ internal/task/user.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"go-demo/internal/service"
+	"go-demo/pkg/gox"
 	"go-demo/pkg/queuex"
 
 	"github.com/hibiken/asynq"
@@ -16,19 +17,27 @@ type user struct{}
 var User user
 
 func (user) AddUser(ctx context.Context, t *asynq.Task) error {
+	// 解析payload
 	var user struct {
 		UserName string `json:"user_name"`
 	}
 	if err := queuex.Payload(&user, t); err != nil {
 		return err
 	}
+
+	// 参数校验
 	if "" == user.UserName {
 		return fmt.Errorf("用户名不得为空. %w", asynq.SkipRetry)
 	}
 
-	userData := map[string]any{
-		"user_name": user.UserName,
+	// 业务处理
+	userData := map[string]any{}
+	if err := gox.TypeCast(user, &userData); err != nil {
+		return err
 	}
-	_, err := service.User.CreateUser(userData)
-	return err
+	if _, err := service.User.CreateUser(userData); err != nil {
+		return err
+	}
+
+	return nil
 }
