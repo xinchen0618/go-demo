e94971270ea546f20007afb8b981b0e203e2b5b0
chenxin <chen.xin@hbtown.com>
2023-04-04 11:07:00 +0800

    rand

--- pkg/gox/rand.go
+++ pkg/gox/rand.go
@@ -7,12 +7,12 @@ import (
 
 // RandInt64 生成区间随机数
 //
-//	左闭右开区间[min, max)
+//	双闭区间[min, max]
 //	@param min int64
 //	@param max int64
 //	@return int64
 func RandInt64(min, max int64) int64 {
-	rand.Seed(time.Now().UnixNano())
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	return rand.Int63n(max-min) + min
+	return r.Int63n(max-min+1) + min
 }
